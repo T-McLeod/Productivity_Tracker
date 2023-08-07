8bf870b616c43b94b5b9b375374d363a0441dead
Tanner89 <tanner89plp@gmail.com>
2023-08-07 16:44:25 -0400

    Created DomainTracker

--- background/TabTracker.js
+++ background/TabTracker.js
@@ -1,11 +1,18 @@
+class DomainTracker {
+    constructor() {
+        this.domains = new Set();
+    }
+
+    addDomain(domain){
+        domains.add(domain);
+        
+    }
+}
+
 chrome.tabs.onActivated.addListener( (activeInfo) => {
     const tabID = activeInfo.tabId
     console.log(activeInfo);
     chrome.tabs.get(tabID, (tab) => {
         TimeLogger.logTime(tab.url);
     })
-    /*chrome.tabs.query({active: true, highlighted: true}, (result) => {
-        console.log(tabID);
-        TimeLogger.logTime(tabID);
-    });*/
 })
