7a19a500d03bfa25af2e74ab4026e65f95036f5a
Tanner89 <tanner89plp@gmail.com>
2023-08-11 16:25:26 -0400

    Added to DomainTracker

--- background/TabTracker.js
+++ background/TabTracker.js
@@ -1,25 +1,41 @@
 class DomainTracker {
+    static numCreated = 0;
+
+
     constructor() {
         this.domains = new Set();
+        this.prefix = "DT" + numCreated + "_";
+        numCreated++;
     }
 
-    addDomain(domain){
+    async addDomain(domain){
         domains.add(domain);
+        domainObject = new DomainObject(domain);
+        
+    }
+
+    addUrl(url){
+        this.addDomain(getDomain(url));
+    }
+
+    async updateDomObj(domainObject){
+        key = prefix + domainObject.domain;
+        //set local storage to key
     }
 
     static getDomain(url){
-        domain = "";
-        while(i < url.length && url.charAt(i) != '/'){
-            domain += url.charAt(i);
-        }
-        return domain;
+        let arr = /(\w+\.)+\w+/.exec(url);
+        if(arr.length == 0)
+            return null;
+        else
+            return arr[0];
     }
 }
 
 class DomainObject {
-    constructor(url) {
-        this.url = url;
-        this.timeSpent
+    constructor(domain) {
+        this.domain = domain;
+        this.timeSpent = 0;
     }
 }
 
